6c6c7701e55ee69328e2963a0afcd887ef46eec1
dongy7 <dongy7@gmail.com>
2016-09-18 23:30:21 -0400

    use configuration to create streams

--- runner.py
+++ runner.py
@@ -6,50 +6,74 @@ import datetime
 import imutils
 import time
 import cv2
+import yaml
+import os.path
 
-# initialize the video streams and allow them to warmup
-print("[INFO] starting cameras...")
-leftStream = VideoStream(src=0).start()
-rightStream = VideoStream(src=1).start()
-time.sleep(2.0)
+config_file = "config/profile.yml"
 
-# initialize the image stitcher, and total number of frames read
-stitcher = Stitcher()
-total = 0
+def setup():
+    if not os.path.isfile(config_file):
+        raise ValueError('Configuration file does not exist.')
+    with open(config_file, 'r') as file:
+        doc = yaml.load(file)
+        left_index = doc["left-index"]
+        right_index = doc["right-index"]
+    return left_index, right_index
 
-# loop over frames from the video streams
-while True:
-	# grab the frames from their respective video streams
-	left = leftStream.read()
-	right = rightStream.read()
+def initialize(left_index, right_index):
+    # initialize the video streams and allow them to warmup
+    print("[INFO] starting cameras...")
+    leftStream = VideoStream(src=left_index).start()
+    rightStream = VideoStream(src=right_index).start()
 
-	# resize the frames
-	left = imutils.resize(left, width=400)
-	right = imutils.resize(right, width=400)
+    return leftStream, rightStream
 
-	# stitch the frames together to form the panorama
-	# IMPORTANT: you might have to change this line of code
-	# depending on how your cameras are oriented; frames
-	# should be supplied in left-to-right order
-	result = stitcher.stitch([left, right])
+def stream(leftStream, rightStream):
+    time.sleep(2.0)
+    stitcher = Stitcher()
 
-	# no homograpy could be computed
-	if result is None:
-		print("[INFO] homography could not be computed")
-		break
+    # loop over frames from the video streams
+    while True:
+        # grab the frames from their respective video streams
+        left = leftStream.read()
+        right = rightStream.read()
 
-	# show the output images
-	cv2.imshow("Result", result)
-	cv2.imshow("Left Frame", left)
-	cv2.imshow("Right Frame", right)
-	key = cv2.waitKey(1) & 0xFF
+        # resize the frames
+        left = imutils.resize(left, width=400)
+        right = imutils.resize(right, width=400)
 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+        # stitch the frames together to form the panorama
+        # IMPORTANT: you might have to change this line of code
+        # depending on how your cameras are oriented; frames
+        # should be supplied in left-to-right order
+        result = stitcher.stitch([left, right])
 
-# do a bit of cleanup
-print("[INFO] cleaning up...")
-cv2.destroyAllWindows()
-leftStream.stop()
-rightStream.stop()
+        # no homograpy could be computed
+        if result is None:
+                print("[INFO] homography could not be computed")
+                break
+
+        # show the output images
+        cv2.imshow("Result", result)
+        cv2.imshow("Left Frame", left)
+        cv2.imshow("Right Frame", right)
+        key = cv2.waitKey(1) & 0xFF
+
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+                break
+
+    # do a bit of cleanup
+    print("[INFO] cleaning up...")
+    cv2.destroyAllWindows()
+    leftStream.stop()
+    rightStream.stop()
+
+
+def main():
+    left_index, right_index = setup()
+    left_stream, right_stream = initialize(left_index, right_index)
+    stream(left_stream, right_stream)
+
+if __name__ == "__main__":
+    main()
