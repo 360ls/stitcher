cbc588b3f76994c4067d985830ac8bcde2686d26
dongy7 <dongy7@gmail.com>
2016-09-23 12:08:53 -0400

    move check for configuration out of runner

--- utils/configuration.py
+++ utils/configuration.py
@@ -1,8 +1,18 @@
 import yaml
+import os.path
 
 class Configuration:
     def __init__(self):
-        with open("config/profile.yml", 'r') as f:
+        self.config_file = "config/profile.yml"
+        self.check_config_file()
+        self.initialize()
+
+    def check_config_file(self):
+        if not os.path.isfile(self.config_file):
+            raise ValueError('Configuration file does not exist.')
+
+    def initialize(self):
+        with open(self.config_file, 'r') as f:
             doc = yaml.load(f)
             self.left_index = doc["left-index"]
             self.right_index = doc["right-index"]
