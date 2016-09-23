3be63f341eff62b4b7575c4223af2ba98c854c41
dongy7 <dongy7@gmail.com>
2016-09-23 10:19:14 -0400

    add configuration for using local images

--- configure.py
+++ configure.py
@@ -5,7 +5,6 @@ config_dir = "config"
 def main():
     parse()
 
-
 def parse():
     try:
         left_index = int(raw_input('Enter index of left camera: '))
@@ -17,13 +16,25 @@ def parse():
     except ValueError:
         print "Please enter a number."
 
-    generate_yaml(left_index, right_index)
+    source_dir = raw_input('Enter full path to image source directory: ')
+    dest_dir = raw_input('Enter full path to image output directory: ')
+    key_frame = raw_input('Enter full path to key frame image: ')
 
+    try:
+        width = int(raw_input('Enter image width: '))
+    except ValueError:
+        print "Please enter a number."
 
-def generate_yaml(left_ix, right_ix):
+    format = raw_input('Enter image format: ')
+
     settings = {
-            'left-index': left_ix,
-            'right-index': right_ix
+            'left-index': left_index,
+            'right-index': right_index,
+            'source-dir': source_dir,
+            'dest-dir': dest_dir,
+            'key-frame': key_frame,
+            'width': width,
+            'format': format
             }
     with open(config_dir + '/profile.yml', 'w') as file:
         yaml.dump(settings, file, default_flow_style=False)
