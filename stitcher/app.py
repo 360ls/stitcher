ab595b6b4ece3a1b6bb13f67311dea5b0b38ad4b
jdwitten <jdwitten11@gmail.com>
2016-10-12 14:26:22 -0400

    fixed piping

--- stitcher/app.py
+++ stitcher/app.py
@@ -151,7 +151,7 @@ def stitch(left_stream, right_stream):
             cv2.imshow("Left Stream", left_frame)
             cv2.imshow("Right Stream", right_frame)
             cv2.imshow("Stitched Stream", result)
-	    proc.stdin(result.tostring())
+	    proc.stdin.write(result.tostring())
             # no homograpy could be computed
             if result is None:
                 Formatter.print_err("[INFO] homography could not be computed")
