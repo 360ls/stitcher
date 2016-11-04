dceb4628122265a48e69c74b7edfc3dab63b70c8
Luke Fernandez <lukejohnfernandez@gmail.com>
2016-11-03 23:11:29 -0400

    added main.js comments and es6 linting

--- app/app.py
+++ app/app.py
@@ -4,13 +4,14 @@ from __future__ import print_function
 import sys
 from flask import Flask
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
-@app.route("/")
+@APP.route("/")
 def hello():
+    """ Displays boilerplate output to the electron application. """
     return "Displaying from Python script."
 
 if __name__ == "__main__":
     print('Python script for main app is active.')
     sys.stdout.flush()
-    app.run()
+    APP.run()
--- main.js
+++ main.js
@@ -1,32 +1,24 @@
-const electron = require('electron');
-const app = electron.app;  // Module to control application life.
-const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+/* jshint esversion: 6 */
+
+const {app, BrowserWindow, crashReporter} = require('electron');
 
 // Report crashes to our server.
-electron.crashReporter.start({
+crashReporter.start({
     productName: 'stitch-flex',
     companyName: 'lukejfernandez',
-    submitURL: 'https://your-domain.com/url-to-submit',
+    submitURL: '',
     autoSubmit: false
 });
 
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the javascript object is GCed.
+// Global reference for the mainWindow to bypass garbage collection
 var mainWindow = null;
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  //if (process.platform != 'darwin') {
-    app.quit();
-  //}
-});
 
 // This method will be called when Electron has done everything
 // initialization and ready for creating browser windows.
 app.on('ready', function() {
-  // call python?
-  var subpy = require('child_process').spawn('python', ['./app/app.py']);
+
+  var python_subprocess = require('child_process').spawn('python', ['./app/app.py']);
 
   var rq = require('request-promise');
   var mainAddr = 'http://localhost:5000';
@@ -41,14 +33,19 @@ app.on('ready', function() {
     // mainWindow.webContents.openDevTools();
     // Emitted when the window is closed.
     mainWindow.on('closed', function() {
-      // Dereference the window object, usually you would store windows
-      // in an array if your app supports multi windows, this is the time
-      // when you should delete the corresponding element.
+      // Dereferences the mainWindow object. Replace with stack, if multiwindow
       mainWindow = null;
-      // kill python
-      subpy.kill('SIGINT');
+      // Kills subprocess opened up to pass Python to Electron
+      python_subprocess.kill('SIGINT');
     });
   };
+
+// Ensures quit on window close, even for OSX
+app.on('window-all-closed', function() {
+  //if (process.platform != 'darwin') {
+    app.quit();
+  //}
+});
 
   var startUp = function(){
     rq(mainAddr)
