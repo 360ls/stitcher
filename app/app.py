e5b5e47ce8db271681fd5051497d394017fc7327
Luke Fernandez <lukejohnfernandez@gmail.com>
2016-11-03 22:47:48 -0400

    added pytest to requirements. updated package.json

--- app/app.py
+++ app/app.py
@@ -1,15 +1,16 @@
+""" Primary handler for python output as input to the electorn application. """
+
 from __future__ import print_function
-from time import sleep
 import sys
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Displaying from Python script"
+    return "Displaying from Python script."
 
 if __name__ == "__main__":
     print('Python script for main app is active.')
-    #sleep(10)
     sys.stdout.flush()
     app.run()
