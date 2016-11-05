d1d2af9de0045e7d96668e70fb32f75898e91d8d
Luke Fernandez <lukejohnfernandez@gmail.com>
2016-11-05 10:13:10 -0400

    pulled in formatter

---
+++ app/cli.py
@@ -0,0 +1,10 @@
+""" Responsible for demonstrating stitching and streaming functionality in the terminal. """
+from __future__ import print_function
+
+def main():
+    return
+
+
+
+if __name__ == "__main__":
+    main()
