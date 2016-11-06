6724c799b76d223b56e1825de1f89359522940d1
Luke Fernandez <lukejohnfernandez@gmail.com>
2016-11-06 11:42:12 -0500

    fixed pytest - working locally

--- app/test/test_inputscanner.py
+++ app/test/test_inputscanner.py
@@ -3,8 +3,8 @@ Module responsible for testing functionality of the inputscanner utility.
 """
 
 from __future__ import absolute_import, division, print_function
-from .util.inputscanner import InputScanner
+from ..util.inputscanner import InputScanner
 
 def test_read_int():
     """ Checks to make sure output of read_int is the same as input. """
-    pass
+    assert 1 == 1
